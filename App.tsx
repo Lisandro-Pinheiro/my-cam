23d5aa4c476b3f6aa859737312c8705e16da78a7
Lisandro-Pinheiro <lisandropinheiro10@gmail.com>
2023-05-29 21:35:49 -0300

    Commit Aula

--- App.tsx
+++ App.tsx
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { Camera, CameraType } from "expo-camera";
+import { useEffect, useState } from "react";
+import { Button, View, StyleSheet } from "react-native";
+import MediaLibrary from "expo-media-library"
 
 export default function App() {
+
+  const [image, setImage] = useState(null);
+  const [camera, setCamera] = useState(null);
+  const [permission, setPermission] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const cameraStatus = await Camera.requestCameraPermissionsAsync();
+      setPermission(cameraStatus.status === 'granted')
+    })();
+  }, []);
+
+  async function TakePicture() {
+    if (camera) {
+      const photo = await camera.takePictureAsync();
+      console.log(photo.uri);
+      await MediaLibrary.saveToLibraryAsync(photo.uri);
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Camera
+        ref={(minhaCamera) => setCamera(minhaCamera)}
+        style={styles.styleCamera}
+        type={CameraType.back}
+        ratio={'1:1'}>
+      </Camera>
+      <Button title="Tirar Foto" onPress={() => {TakePicture() }} />
     </View>
   );
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+
+  styleCamera: {
+    aspectRatio: 1,
+    flex: 1
+  }
+
+
 });
