780f06462fe5f4bb252f126ec94f11607d88a225
Lisandro-Pinheiro <lisandropinheiro10@gmail.com>
2023-05-30 20:58:56 -0300

    2º Commit

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
 
 import { Camera, CameraType } from "expo-camera";
-import { useEffect, useState } from "react";
-import { Button, View, StyleSheet } from "react-native";
-import MediaLibrary from "expo-media-library"
+import React, { useEffect, useState } from "react";
+import {View, StyleSheet, TouchableHighlight, Text } from "react-native";
+import * as MediaLibrary from "expo-media-library";
+import { Image } from 'expo-image';
 
 export default function App() {
 
@@ -14,6 +15,7 @@ export default function App() {
     (async () => {
       const cameraStatus = await Camera.requestCameraPermissionsAsync();
       setPermission(cameraStatus.status === 'granted')
+      await MediaLibrary.requestPermissionsAsync();
     })();
   }, []);
 
@@ -21,19 +23,30 @@ export default function App() {
     if (camera) {
       const photo = await camera.takePictureAsync();
       console.log(photo.uri);
+      setImage(photo.uri)
       await MediaLibrary.saveToLibraryAsync(photo.uri);
     }
   }
-
   return (
     <View style={styles.container}>
       <Camera
         ref={(minhaCamera) => setCamera(minhaCamera)}
         style={styles.styleCamera}
         type={CameraType.back}
-        ratio={'1:1'}>
-      </Camera>
-      <Button title="Tirar Foto" onPress={() => {TakePicture() }} />
+        ratio={'1:1'} />
+      <Image
+        style={styles.container}
+        source={image}
+        contentFit="cover"
+        transition={1000} />
+      <TouchableHighlight
+        style={styles.button}
+        onPress={() => { TakePicture() }}>
+        <Text
+          style={{ color: '#fff', fontSize: 25 }}>
+          Tirar Foto
+        </Text>
+      </TouchableHighlight>
     </View>
   );
 
@@ -42,11 +55,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
 
   styleCamera: {
     aspectRatio: 1,
     flex: 1
+  },
+
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    elevation: 3,
+    backgroundColor: "#6676f1",
+    width: 100,
+    height: 100,
+    borderRadius: 100,
+    position: "absolute",
+    bottom: 50
+
   }
 
 
